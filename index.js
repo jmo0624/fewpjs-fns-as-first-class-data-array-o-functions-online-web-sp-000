9af31b8f58c4efdb6fa7373042a44ba25d65f8cd
Josh Morris <jmo0624@gmail.com>
2020-07-14 23:44:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-
-function dogcare() {
-  let dogName = "Byron";
-  let dogBreed = "poodle";
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-  console.log(`Leash ${dogName} the ${dogBreed}`);
-  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
-  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
-  console.log(`Walk home with ${dogName} the ${dogBreed}`);
-  console.log(`Unleash ${dogName} the ${dogBreed}`);
-}
 function dogcare(dogName, dogBreed) {
 
   console.log(`Wake ${dogName} the ${dogBreed}`);
